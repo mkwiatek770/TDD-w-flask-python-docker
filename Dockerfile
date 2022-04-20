cc031a0508837e1bb3cb08f871b2c8c17c7cffad
mkwiatek770 <michalakwiatek@gmail.com>
2022-04-20 11:22:43 +0200

    setup postgresql

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,14 @@ WORKDIR /usr/src/app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+RUN apt-get update \
+    && apt-get -y install netcat gcc postgresql \
+    && apt-get clean
+
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD python manage.py run -h 0.0.0.0
+COPY ./entrypoint.sh .
+RUN chmod +x /usr/src/app/entrypoint.sh
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -10,6 +11,18 @@ api = Api(app)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+
+#model
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username= db.Column(db.String(128), nullable=False)
+    email= db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
 
 
 class Ping(Resource):
