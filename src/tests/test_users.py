6a09dc501c9fc113ae3230b85e98626b921727f4
mkwiatek770 <michalakwiatek@gmail.com>
2022-04-25 21:34:35 +0200

    more tests

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -14,3 +14,56 @@ def test_add_user(test_app, test_database):
 
     assert resp.status_code == 201
     assert 'michael@testdriven.io was added!' in data['message']
+
+
+def test_add_user_no_username(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'email': 'email@w.com',
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_no_email(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'someuser',
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'That email already exists!' in data['message']
