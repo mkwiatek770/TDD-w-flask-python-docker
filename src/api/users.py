69d433ae039c5813dc571ff47d820494013a3373
mkwiatek770 <michalakwiatek@gmail.com>
2022-04-25 21:34:49 +0200

    user validation

--- src/api/users.py
+++ src/api/users.py
@@ -1,6 +1,6 @@
 from curses import use_env
 from flask import Blueprint, request
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
 
 from src import db
 from src.api.models import User
@@ -10,12 +10,27 @@ users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
 
 
+user = api.model('User', {
+    'id': fields.Integer(readOnly=True),
+    'username': fields.String(required=True),
+    'email': fields.String(required=True),
+    'created_date': fields.DateTime,
+})
+
+
 class UsersList(Resource):
 
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get('username')
         email = post_data.get('email')
+        response_object = dict()
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            response_object['message'] = 'That email already exists!'
+            return response_object, 400
 
         db.session.add(User(username=username, email=email))
         db.session.commit()
